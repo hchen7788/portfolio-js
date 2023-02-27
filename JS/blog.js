a6e6bdac29f15eb9260374eabaaa641aed964a00
HaitongC <haitongchen116@gmail.com>
2023-02-26 16:47:33 -0800

    prepopulate

---
+++ JS/blog.js
@@ -0,0 +1,8 @@
+let allBlogs = document.getElementsByClassName('crudBlog');
+allBlogs = Array.from(allBlogs)
+
+console.log(allBlogs.length)
+
+for(let i = 0; i < allBlogs.length; i++){
+    console.log("hi")
+}
