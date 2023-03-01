a6a5549f0fd840ff8f334576a162f934c086a8fa
HaitongC <haitongchen116@gmail.com>
2023-02-28 17:18:25 -0800

    event loop working for all

--- JS/builtin.js
+++ JS/builtin.js
@@ -6,37 +6,61 @@ function confirmTag(result){
 }
 
 function readFromConfirm(){
-    confirmTag(confirm("Do you confirm this?"));
+    clearContent()
+    
+    setTimeout(() => {
+        confirmTag(confirm("Do you confirm this?"));
+    }, 0);
 }
 
 function promptTag(result){
     document.getElementById("out").innerHTML = `${promptMessage}${result}`;
 }
 
-function unsafePrompt(){
-    let output = document.getElementById("out")
-    let name = prompt("What is your name?");
+// function unsafePrompt(){
+//     let output = document.getElementById("out")
+//     let name = prompt("What is your name?");
 
+//     setTimeout(() => {
+//     //let name = prompt("What is your name?");
+//     if(name != null){
+//         promptTag(name);
+//     } else{
+//         output.innerHTML = "User didn't enter anything";
+//     }
+// }, 0);
+// }
+
+function unsafePrompt(){
+    let output = document.getElementById("out");
+    clearContent()
+    
     setTimeout(() => {
-    //let name = prompt("What is your name?");
-    if(name != null){
-        promptTag(name);
-    } else{
-        output.innerHTML = "User didn't enter anything";
-    }
-}, 0);
+        let name = prompt("What is your name?");
+        if(name != null){
+            promptTag(name);
+        } else{
+            output.innerHTML = "User didn't enter anything";
+        }
+    }, 0);
 }
 
 function safePrompt(){
     let name = prompt("What is your name?");
-    let clean = DOMPurify.sanitize(name, { USE_PROFILES: { html: true } });
+    clearContent()
+    
+    setTimeout(() => {
+        let clean = DOMPurify.sanitize(name, { USE_PROFILES: { html: true } });
     if(name != null){
         promptTag(clean);
     } else{
         document.getElementById("out").innerHTML = "User didn't enter anything";
     }
+    }, 0);
 }
 
 function clearContent() {
     document.getElementById("out").innerHTML = "";
 }
+
+// <b onmouseover="alert('pwned')">Roll me</b>
