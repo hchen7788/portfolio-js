fc912a524142901f03264eaa8193131f5217a6f8
HaitongC <haitongchen116@gmail.com>
2023-03-01 22:15:42 -0800

    fix submit function

--- JS/framework.js
+++ JS/framework.js
@@ -3,15 +3,8 @@ let feedback
 function showFeedbackDialog(){
     let dia = document.getElementById("feedbackDialog")
     dia.showModal()
+}
 
-    let submitBtn = document.getElementById('submitFromFeedback')
-    
-    submitBtn.addEventListener('click', () => {
-        let nameInput = document.getElementById('feedbackNameInput').value
-        let contactInput = document.getElementById('feedbackContactInput').value
-        let contentInput = document.getElementById('feedbackContentInput').value
-
-        //feedback.push({nameInput, contactInput, contentInput});
-        alert("Thank you for your input!")
-    })
+function submitFeedback(){
+    alert("Thank you for your input!")
 }
